8d48a1a6b072c90cfa9094ac057e9449b0e3df63
Thomas Russell <t.p.russell19@gmail.com>
2021-05-31 15:46:19 -0700

    :hammer: update models to handle many to many relationship

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,12 +4,12 @@ namespace Factory.Models
 {
   public class Engineer
   {
-    public Engineer()
-    {
-      this.Machines = new HashSet<Machine>();
-    }
     public int EngineerId { get; set; }
     public string Name { get; set; }
-    public virtual ICollection<Machine> Machines { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,10 +6,6 @@ namespace Factory.Models
   {
     public int MachineId { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
-    public int Price { get; set; }
-    public string Location { get; set; }
-    public int EngineerId { get; set; }
-    public virtual Engineer Engineer { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
   }
 }
